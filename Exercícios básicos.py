0e512983beb86dc0c500a6871c630bfe324b6ac4
dudaswc <125403834+dudaswc@users.noreply.github.com>
2023-02-15 15:32:41 -0300

    Update Exercícios básicos.py

--- Exercícios básicos.py
+++ Exercícios básicos.py
@@ -17,3 +17,37 @@ soma=valor1+valor2
 dobro=(valor1+valor2)*2
 print("soma:", soma)
 print("dobro:", dobro)
+
+
+3- O restaurante “Tudo Gostoso” cobra R$ 30.00 pelo quilo de refeição. Escreva um
+programa que lê o peso do prato cheio de um cliente (em quilos) e imprima o valor a
+pagar.
+* Considere que o peso do prato é 0.1 Kg.
+
+kg= float(input("kg"))
+pkg=30
+total=float(pkg-0.8)*kg
+print("o total a pagar é:", total)
+
+
+4- Antes do racionamento de energia ser decretado, quase ninguém falava em quilowatts, mas agora, todos a incorporaram a seu vocabulário. Sabendo que cada quilowatt de energia custa um centésimo do salário mínimo, escreva um algoritmo que receba o valor do salário mínimo e a quantidade de quilowatts gasta em uma residência, calcule e imprima o valor pago pela residência em questão.
+
+salario=float(input("salario"))
+valorkw=salario/100
+gasto=float(input("gasto"))
+total=gasto*valorkw
+print("total a pagar:", total)
+
+
+5- Uma fábrica produz dois tipos de clips, plástico ou metal, sendo que a caixa de cada tipo é vendida por 5 e 10 reais, respectivamente. Faça um algoritmo em que o usuário forneça a quantidade de caixas vendidas de clips de plástico e de metal e informe qual será o valor arrecadado com cada um dos tipos e o valor total arrecadado.
+
+metal=10
+plastico=5
+vendasm= int(input("informe a quantidade de metal vendido"))
+vendasp=int(input("informe a quantidade de plastico vendido"))
+totalm=vendasm*metal
+totalp=vendasp*plastico
+total=totalm+totalp
+print(totalm)
+print(totalp)
+print(total)
